d6179fb5c4359e5d9a3e912fc0dd6afe70ec6a44
kiplangat meshack <meshnobie@gmail.com>
2023-04-24 14:37:35 +0300

    created dictinary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+# no duplicate keys 
+# cant use mutable objects as keys
+# can be nested
+# dictionaries are unordered
+
+dic = {"name":"kiplangat", "school":"kca university","email":"meshnobie@gmail.com"}
+
+print(dic)
+
+
