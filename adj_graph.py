b1f67e5249bcd9a06e7d3838ad6cd4d7c0d85d1d
kiplangat01 <meshnobie@gmail.com>
2023-05-08 21:17:54 +0300

    added delete function

--- adj_graph.py
+++ adj_graph.py
@@ -1,22 +1,34 @@
 nodes = []
 node_count = 0
 graph = []
+
+
 def add_node(v):
+    global nodes,node_count,graph
     if v in graph:
         print(v,"already present`")
     else:
-        graph[v] = []
+        node_count = node_count + 1
+        nodes.append(v)
+        for n in graph:
+            n.append(0)
+        temp = []
+        for i in range(node_count):
+            temp.append(0)
+        graph.append(temp)
 
-def add_edge(v1,v2,cost):
+
+def add_edge(v1,v2,e):
+    global graph
     if v1 not in graph:
-        print(v1,"absent in graph")
+        print("absent in graph")
     elif v2 not in graph:
-        print(v2,"absent in graph")
+        print("absent in graph")
     else:
-        list1 = [v2,cost]
-        list2 = [v1,cost]
-        graph[v1].append(list1)
-        graph[v2].append(list2)
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
+        graph[index1][index2] = e
+        graph[index2][index1] = e
 
 
 def delete_node(v):
@@ -30,6 +42,11 @@ def delete_node(v):
         for i in graph:
             i.pop(index1)
 
+def print_graph():
+    for i in range(node_count):
+        for j in range(node_count):
+            print(format(graph[i][j],"<3",end=" "))
+        print()
 
 
 
@@ -37,4 +54,6 @@ add_node("A")
 add_node("B")
 add_node("C")
 add_edge("A","B",10)
+add_edge("A","C",10)
+delete_node("A")
 print(graph)
